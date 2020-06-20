81351b754dc37a133f373e7671b1b3af877204ff
SamirIngley <samir.ingle7@gmail.com>
2020-06-20 00:05:42 -0700

    rest of code to tell you how many bears

--- content.js
+++ content.js
@@ -2,4 +2,5 @@ alert('Grrr.')
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     const re = new RegExp('bear', 'gi')
     const matches = document.documentElement.innerHTML.match(re)
+    sendResponse({count: matches.length})
 })
