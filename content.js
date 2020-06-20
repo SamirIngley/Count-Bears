b80d485a8727a10e950a83dd78179c3c6bf35581
SamirIngley <samir.ingle7@gmail.com>
2020-06-20 00:20:52 -0700

    opens a separate tab and displays number of times 'bear' shows up in each of them

--- content.js
+++ content.js
@@ -1,6 +1,17 @@
-alert('Grrr.')
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    const re = new RegExp('bear', 'gi')
-    const matches = document.documentElement.innerHTML.match(re)
-    sendResponse({count: matches.length})
+// alert('Grrr.')
+
+// WITH BUTTON WILL FIND NUMBER OF BEARS
+// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+//     const re = new RegExp('bear', 'gi')
+//     const matches = document.documentElement.innerHTML.match(re)
+//     sendResponse({count: matches.length})
+// })
+
+// AUTOMATICALLY FINDS NUMBER OF BEARS
+
+const re = new RegExp('bear', 'gi')
+const matches = document.documentElement.innerHTML.match(re)
+chrome.runtime.sendMessage({
+    url: window.location.href,
+    count: matches.length
 })
--- popup.js
+++ popup.js
@@ -1,18 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('button').addEventListener('click',
-    onclick, false)
 
-    function onclick () {
-        chrome.tabs.query({currentWindow: true, active: true},
-        function (tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, 'hi', setCount)
-        })
-    }
-
-    function setCount (res) {
+    // GET BACKGROUND PAGE
+    const bg = chrome.extension.getBackgroundPage()
+    // now we have access (below command) to the background js bears
+    Object.keys(bg.bears).forEach(function(url) {
         const div = document.createElement('div')
-        div.textContent = `${res.count} bears`
+        div.textContent = `${url}: ${bg.bears[url]}`
         document.body.appendChild(div)
-    }
+    })
+
+
+    // document.querySelector('button').addEventListener('click',
+    // onclick, false)
+
+    // function onclick () {
+    //     chrome.tabs.query({currentWindow: true, active: true},
+    //     function (tabs) {
+    //         chrome.tabs.sendMessage(tabs[0].id, 'hi', setCount)
+    //     })
+    // }
+
+    // function setCount (res) {
+    //     const div = document.createElement('div')
+    //     div.textContent = `${res.count} bears`
+    //     document.body.appendChild(div)
+    // }
 
 }, false)
