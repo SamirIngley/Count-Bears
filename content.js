5fc8911f07e6ba9fe1147c0b2bd38053f582aab2
SamirIngley <samir.ingle7@gmail.com>
2020-06-20 00:03:30 -0700

    sender, sendResponse allow us to respond with how many instances there are on page

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 alert('Grrr.')
-chrome.runtime.onMessage.addListener(function(request) {
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     const re = new RegExp('bear', 'gi')
     const matches = document.documentElement.innerHTML.match(re)
 })
