7bea898e490014f404368d23de466fc9c02ed430
SamirIngley <samir.ingle7@gmail.com>
2020-06-20 00:02:41 -0700

    should find all instances of bear on page

--- content.js
+++ content.js
@@ -1,4 +1,5 @@
 alert('Grrr.')
 chrome.runtime.onMessage.addListener(function(request) {
-    alert(request)
+    const re = new RegExp('bear', 'gi')
+    const matches = document.documentElement.innerHTML.match(re)
 })
